998f9185f14c2197a0bc2b68588c40c6f59832ab
Kwenoth <kwenoth@gmail.com>
2022-06-21 05:01:08 +0400

    Added Eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const user = () => { 
+const user = () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + name + '!');
+  console.log(`Hello, ${name}!`);
 };
+
+export default user;
