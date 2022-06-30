f56648146fe710d45651bd6b6ce58e187ce5e7b4
Kwenoth <kwenoth@gmail.com>
2022-06-30 14:38:58 +0400

    Fixed array and answer generation

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,19 +3,17 @@ import getRandomInt from '../get-random-Int.js';
 
 const rule = 'What number is missing in the progression?';
 
-const getCollOfNums = (start, weight, step, index) => {
+const generateProgression = (start, weight, step, index) => {
   const coll = [];
-  let elementValue;
   for (let i = 0; i < weight; i += 1) {
     if (i === index) {
       coll.push('..');
-      elementValue = i * step + start;
     } else {
       coll.push(i * step + start);
     }
   }
 
-  return [coll, elementValue];
+  return coll;
 };
 
 const progressionLogic = () => {
@@ -23,11 +21,12 @@ const progressionLogic = () => {
   const startNum = getRandomInt(0, 100);
   const stepOfNums = getRandomInt(1, 5);
   const randomIndex = getRandomInt(0, collWeight - 1);
+  const answer = randomIndex * stepOfNums + startNum;
 
-  const collOfNums = getCollOfNums(startNum, collWeight, stepOfNums, randomIndex);
+  const collOfNums = generateProgression(startNum, collWeight, stepOfNums, randomIndex);
 
-  const gameQuestion = collOfNums[0].join(' ');
-  const gameAnswer = collOfNums[1].toString();
+  const gameQuestion = collOfNums.join(' ');
+  const gameAnswer = answer.toString();
 
   return [gameQuestion, gameAnswer];
 };
