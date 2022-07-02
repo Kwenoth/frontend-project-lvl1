76a1a513fa95808ae807f21eee83c4ebb24021f6
Kwenoth <kwenoth@gmail.com>
2022-07-02 17:29:42 +0400

    Renamed isEvenNum to isEven

--- src/games/is-even.js
+++ src/games/is-even.js
@@ -3,11 +3,11 @@ import getRandomInt from '../get-random-Int.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEvenNum = (num) => num % 2 === 0;
+const isEven = (num) => num % 2 === 0;
 
 const evenLogic = () => {
   const roundQuestion = getRandomInt(0, 100);
-  const roundAnswer = isEvenNum(roundQuestion) ? 'yes' : 'no';
+  const roundAnswer = isEven(roundQuestion) ? 'yes' : 'no';
 
   return [roundQuestion, roundAnswer];
 };
