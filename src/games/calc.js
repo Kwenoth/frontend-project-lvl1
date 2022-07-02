50351e7fccdf3eba81ca6fb0a4c27f21a1fff2e1
Kwenoth <kwenoth@gmail.com>
2022-07-02 16:31:42 +0400

    Renamed znak to operands (operator)

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,31 +2,28 @@ import commonLogic from '../index.js';
 import getRandomInt from '../get-random-Int.js';
 
 const rule = 'What is the result of the expression?';
-const arrZnak = ['+', '-', '*'];
+const operands = ['+', '-', '*'];
 
 const doCalculation = (x, operator, y) => {
   switch (operator) {
     case '+':
       return x + y;
-
     case '-':
       return x - y;
-
     case '*':
       return x * y;
-
     default:
       return NaN;
   }
 };
 
 const calcLogic = () => {
-  const randomZnak = getRandomInt(0, arrZnak.length - 1);
-  const znak = arrZnak[randomZnak];
+  const randomOperand = getRandomInt(0, operands.length - 1);
+  const operand = operands[randomOperand];
   const firstNum = getRandomInt(0, 100);
   const secondNum = getRandomInt(0, 100);
-  const gameQuestion = `${firstNum} ${znak} ${secondNum}`;
-  const gameAnswer = doCalculation(firstNum, znak, secondNum).toString();
+  const gameQuestion = `${firstNum} ${operand} ${secondNum}`;
+  const gameAnswer = doCalculation(firstNum, operand, secondNum).toString();
 
   return [gameQuestion, gameAnswer];
 };
